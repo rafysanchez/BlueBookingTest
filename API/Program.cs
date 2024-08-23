9f8baf595a9f459d957cf99bdeb08457261ae329
rafael sanchez port <rafysanchez@hotmail.com>
2024-08-23 18:05:46 -0300

    apis-0

---
+++ API/Program.cs
@@ -0,0 +1,55 @@
+﻿using WebApi.Authorization;
+using WebApi.Helpers;
+using WebApi.Services;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to DI container
+{
+    var services = builder.Services;
+    services.AddCors();
+    services.AddControllers();
+
+    // configure strongly typed settings object
+    services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+    // configure DI for application services
+    services.AddScoped<IJwtUtils, JwtUtils>();
+    services.AddScoped<IUserService, UserService>();
+
+    // configure Swagger
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    });
+}
+
+var app = builder.Build();
+
+// configure HTTP request pipeline
+{
+    // global cors policy
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    // custom jwt auth middleware
+    app.UseMiddleware<JwtMiddleware>();
+
+    // enable middleware to serve generated Swagger as a JSON endpoint
+    app.UseSwagger();
+
+    // enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // serve the Swagger UI at the app's root
+    });
+
+    app.MapControllers();
+}
+
+app.Run("http://localhost:4000");
---
+++ API/README.md
@@ -0,0 +1,5 @@
+# dotnet-7-jwt-authentication-api
+
+.NET 7.0 - JWT Authentication API
+
+Documentation at https://jasonwatmore.com/net-7-csharp-jwt-authentication-tutorial-without-aspnet-core-identity
